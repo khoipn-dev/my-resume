ca3072a667298580f0b9828e39e3e58f1b4b67f1
Lee Yan Ji <leeyanji@Lees-MacBook-Pro.local>
2020-02-05 15:46:27 +0700

    Updates

---
+++ precache-manifest.2a942a3092574afa0a1d481c5c1e71fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d67537d25d7d6a26cf462dd7b904fc9",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "133dd49581178c6f18e3",
+    "url": "/my-resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d760cf861c832ce93af0",
+    "url": "/my-resume/static/js/2.88fa8eb8.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/my-resume/static/js/2.88fa8eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133dd49581178c6f18e3",
+    "url": "/my-resume/static/js/main.28e013ec.chunk.js"
+  },
+  {
+    "revision": "a28fceb482e24d76442f",
+    "url": "/my-resume/static/js/runtime-main.f6aa34f0.js"
+  }
+]);
